cfd01bd511988a5c9138c5185069d29ddeb72ab0
Nikolai94 <nikolai@cybermanager.dk>
2015-05-18 10:24:41 +0200

    rest test

--- server/facade/facade.js
+++ server/facade/facade.js
@@ -1,17 +1,66 @@
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
 var Gruppe = mongoose.model('Gruppe');
+var async = require("async");
+var request = require("request");
 
-function getGrupper(callback){
+
+function getGrupper(fra,til,dateUtc,callback){
+
     Gruppe.find({}, function(err, data){
 
+
         if(err){return callback(err);}
-        return callback(null ,data);
+
+        var tasks = [];
+        var arr = [];
+
+        for(var i =0; i<data.length; i++){
+            tasks.push(getRequestFunction({
+                url: data[i].link+"api/flights/"+fra+"/"+dateUtc,
+                method: 'GET',
+                json: true
+            }));
+
+        }
+
+
+
+        async.parallel(tasks,function(err, results){
+            if(err){console.log(err)};
+       // console.log(results);
+            for(var i=0; i<results.length; i++){
+
+                if(results[i] != undefined){
+                    for(var j=0; j<results[i].length;j++){
+                        arr.push({"airline":results[i][j].airline,"price":results[i][j].price,"flightId":results[i][j].flightId,"takeOffDate":results[i][j].takeOffDate,"landingDate":results[i][j].landingDate,"depature":results[i][j].depature,"destination":results[i][j].destination,"seats":results[i][j].seats,"avaiableSeats":results[i][j].avaiableSeats,"bookingCode":results[i][j].bookingCode,Gruppe:3});
+                    }
+                }
+            }
+            return callback(null ,arr);
+        });
+
+
 
     });
 }
 
 
+
+function getRequestFunction(requestObj)
+{
+    return (function(callback)
+    {
+        request(requestObj, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                callback(null, body);
+            }
+            else {
+                callback(error);
+            }
+        });
+    });
+}
 
 
 
---
+++ test/backend-tests/test.js
@@ -0,0 +1,60 @@
+global.TEST_DATABASE = "mongodb://localhost/TestDataBase_xx1243";
+
+
+var should = require("should");
+var app = require("../../server/app");
+var http = require("http");
+var testPort = 9999;
+var testServer;
+var mongoose = require("mongoose");
+var User = mongoose.model("User");
+var Gruppe = mongoose.model("Gruppe");
+var nock = require("nock");
+var facede = require("../../server/facade/facade");
+var URL = "http://localhost:3000";
+
+
+
+describe("API get flights",function(){
+    before(function(done){
+        global.SKIP_AUTHENTICATION = true;  //Skip security
+        nock.enableNetConnect();
+        var array = [{"airline":"FFn airlines","price":1337.37,"flightId":"22","takeOffDate":1430784000000,"landingDate":1430784000000,"depature":"CPH","seats":5,"bookingCode":false,"Gruppe":3},{"airline":"Lyn airlines","price":1000.37,"flightId":"28","takeOffDate":1430784000000,"landingDate":1430784000000,"depature":"CPH","seats":5,"bookingCode":false,"Gruppe":3}];
+        nock(URL).get('/userApi/CPH/LON/05-01-2017').reply(200,array);
+        done();
+    });
+
+
+    beforeEach(function(done){
+        Gruppe.remove({}, function ()
+        {
+            var arrayNew = [{"gruppeNavn" : "Gruppe1", "link" : "http://smsproject-schultz.rhcloud.com/smsSemProject/"},
+               {"gruppeNavn" : "Gruppe2", "link" : "http://smsproject-schultz.rhcloud.com/smsSemProject/"}];
+
+
+            Gruppe.create(arrayNew,function(err,data){
+                //if(err){ console.log(err);}
+                //else{ console.log(data);}
+                done();
+            });
+        });
+    })
+
+
+
+
+    describe("get Methods",function(){
+        it("should get six flights",function(done){
+            facede.getGrupper("CPH","LON","1430784000000",function(err,result){
+                if(err){ console.log(err);}
+              //  console.log(result[0] + " result")
+                result.length.should.equal(6);
+                done();
+            });
+        })
+    })
+    after(function(done){
+        nock.restore();
+        done();
+    })
+})
